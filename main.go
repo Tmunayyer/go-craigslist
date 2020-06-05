74598a9d29dd91228aa10eb6b0ee3e4cf9d713e4
tmunayyer <tmunayyer@gmail.com>
2020-06-05 14:09:45 -0400

    no errors

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
 	c := Client{}
 	_, err := c.ListCategories()
 	noErr(err)
 	locations, err := c.ListLocations()
 	noErr(err)
+
+	fmt.Println(locations)
 }
